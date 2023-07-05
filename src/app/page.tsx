a8c240f4dda32dda91084fec3e1292d6a2ce9a1c
IgorZibetti <zibettiigor@gmail.com>
2023-07-05 19:57:07 -0300

    text-white

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-   <div className="h-screen bg-gray-950 text-3xl flex items-center justify-center">
+   <div className="h-screen text-white font-black bg-gray-950 text-3xl flex items-center justify-center">
      Hellow Igor
    </div>
   )
